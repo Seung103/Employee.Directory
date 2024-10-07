3aaa643350697cb0ac34d501b800b3962e9d7335
Seung-Woo Seo <seungwoo103@gmail.com>
2024-10-07 14:49:45 -0400

    created working Requests on http file except random employee. trying to figure out random

--- server.js
+++ server.js
@@ -1,1 +1,36 @@
 // TODO: this file :)
+const express = require("express");
+const app = express();
+
+const employees = require("./employees");
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello employees!");
+});
+
+app.get("/employees", (req, res)=> {
+    res.json(employees);
+});
+
+app.get("/employees/:index", (req, res) => {
+    const { index } = req.params;
+    if (index < 0 || index >= employees.length) {
+        res.status(404).send("That employee does not exist.");
+    } else {
+        res.json(employees[index]);
+    }
+});
+
+app.get("/employees/:index", (req, res) => {
+    const { index } = req.params;
+    if (index < 0 || index >= employees.random) {
+    };
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}.`);
+});
+
+
